333737494d464995d3f06fbe1809a38683ea57f0
Cuong Thieu <dev.cuongtt@gmail.com>
2024-04-08 19:30:40 +0700

    update

--- gateway/routes/index.js
+++ gateway/routes/index.js
@@ -1,13 +1,17 @@
 const express = require("express");
 
+const productRoutes = require("./product.routes");
 const authRoutes = require("./auth.routes");
 const userRoutes = require("./user.routes");
 const categoryRoutes = require("./category.routes");
+const orderRoutes = require("./order.routers");
 
 const router = express.Router();
 
+router.use("/product", productRoutes);
 router.use("/", authRoutes);
 router.use("/", userRoutes);
 router.use("/category", categoryRoutes);
+router.use("/order", orderRoutes);
 
 module.exports = router;
