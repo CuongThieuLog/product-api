fc0584eb162923d3e32efff9ec05cbff77c5c752
Cuong Thieu <dev.cuongtt@gmail.com>
2024-04-13 21:41:52 +0700

    update

--- gateway/routes/user.routes.js
+++ gateway/routes/user.routes.js
@@ -33,14 +33,19 @@ router.get("/user-me", async (req, res) => {
 
 router.get("/user", async (req, res) => {
   try {
+    let headers = {};
+
+    if (req.headers.authorization) {
+      headers = {
+        Authorization: req.headers.authorization.replace("Bearer ", ""),
+      };
+    }
     const params = req.query;
     let apiUrl = "http://localhost:8002/user";
     if (params.name) {
       apiUrl += `?name=${params.name}`;
     }
-    const userAllRes = await axios.get(apiUrl, {
-      headers: { Authorization: req.headers.authorization },
-    });
+    const userAllRes = await axios.get(apiUrl, { headers });
     res.json(userAllRes.data);
   } catch (error) {
     res.status(500).json(error);
